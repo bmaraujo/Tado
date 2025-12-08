ef3eba9d1d0016b6079a40d1c155b95ebe0c3379
Bruno Araújo <8039621+bmaraujo@users.noreply.github.com>
2025-12-08 10:39:47 -0300

    CDK project created

---
+++ test/tado.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tado from '../lib/tado-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tado-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tado.TadoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
